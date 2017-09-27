4c23dd1e69c26915b2d7b86eba4ae6961a6c758e
Pavel Dovgaluk <pavel.dovgaluk@gmail.com>
2017-09-27 08:48:48 +0300

    Remapped YM pins

--- game/ym2149.cpp
+++ game/ym2149.cpp
@@ -4,30 +4,31 @@
 #include <avr/io.h>
 
 // +5V is connected to BC2
-// MSB (PA1) is connected to BDIR
-// LSB (PA0) is connected to BC1
-#define DATA_READ 0x01
-#define DATA_WRITE 0x02
-#define ADDRESS_MODE 0x03
+// MSB (A7) is connected to BDIR
+// LSB (A6) is connected to BC1
+#define DATA_READ (0x01 << 6)
+#define DATA_WRITE (0x02 << 6)
+#define ADDRESS_MODE (0x03 << 6)
+#define MASK 0xc0
 
 void music_setup(void) {
-  DDRA |= 3;
+  DDRF |= MASK;
   DDRC = 0xff;
 }
 
 static void set_address(char addr) {
-  PORTA = (PORTA & ~3) | ADDRESS_MODE;
+  PORTF = (PORTF & ~MASK) | ADDRESS_MODE;
   PORTC = addr;
   _delay_us(1.); //tAS = 300ns
-  PORTA = (PORTA & ~3); // inactive
+  PORTF = (PORTF & ~MASK); // inactive
   _delay_us(1.); //tAH = 80ns
 }
 
 static void set_data(uint8_t data) {
-  PORTA = (PORTA & ~3) | DATA_WRITE;
+  PORTF = (PORTF & ~MASK) | DATA_WRITE;
   PORTC = data;
   _delay_us(1.); // 300ns < tDW < 10us
-  PORTA = (PORTA & ~3); // inactive
+  PORTF = (PORTF & ~MASK); // inactive
   _delay_us(1.); // tDH = 80ns
 }
 
