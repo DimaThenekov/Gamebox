9cb2c967bc67aa7cf5c7c0c5bf3f485f9fcc2048
Pavel Dovgaluk <pavel.dovgaluk@gmail.com>
2018-05-23 15:27:20 +0300

    Added missing fxm files

---
+++ game/fxm_tunes.h
@@ -0,0 +1,44 @@
+#ifndef FXM_TUNES_H
+#define FXM_TUNES_H
+
+#include <Arduino.h>
+
+extern const uint8_t alla_turca[] PROGMEM;
+extern const uint8_t atomix[] PROGMEM;
+extern const uint8_t axel_f_theme[] PROGMEM;
+extern const uint8_t aztec[] PROGMEM;
+extern const uint8_t belegost[] PROGMEM;
+extern const uint8_t chimera[] PROGMEM;
+extern const uint8_t commando[] PROGMEM;
+extern const uint8_t crazy_comets_and_penetrator[] PROGMEM;
+extern const uint8_t e_t_flying[] PROGMEM;
+extern const uint8_t eqinoxe_v[] PROGMEM;
+extern const uint8_t exploding_atoms[] PROGMEM;
+extern const uint8_t falcon[] PROGMEM;
+extern const uint8_t feud[] PROGMEM;
+extern const uint8_t fire[] PROGMEM;
+extern const uint8_t fugue[] PROGMEM;
+extern const uint8_t ghostbusters[] PROGMEM;
+extern const uint8_t golden_triangle_anthem[] PROGMEM;
+extern const uint8_t indiana_jones_3[] PROGMEM;
+extern const uint8_t jet_story[] PROGMEM;
+extern const uint8_t land_of_confusion[] PROGMEM;
+extern const uint8_t magnetic_fields_iv[] PROGMEM;
+extern const uint8_t magnetic_fields_v[] PROGMEM;
+extern const uint8_t master_of_magic[] PROGMEM;
+extern const uint8_t monty_on_the_run[] PROGMEM;
+extern const uint8_t piskworks_topgun[] PROGMEM;
+extern const uint8_t red_dawn[] PROGMEM;
+extern const uint8_t rendezvous_iv[] PROGMEM;
+extern const uint8_t space_crusade[] PROGMEM;
+extern const uint8_t stardragon[] PROGMEM;
+extern const uint8_t stardragon_hall_of_fame[] PROGMEM;
+extern const uint8_t terra_cresta[] PROGMEM;
+extern const uint8_t terra_cresta_[] PROGMEM;
+extern const uint8_t tetris2[] PROGMEM;
+extern const uint8_t the_last_v8[] PROGMEM;
+extern const uint8_t view_to_a_kill[] PROGMEM;
+extern const uint8_t where_time_dropped_dead[] PROGMEM;
+extern const PROGMEM uint8_t zub[];
+
+#endif
