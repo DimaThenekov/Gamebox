d3a36dfb3e212e504c0874a51799a8dfd2d7411d
Pavel Dovgalyuk <pavel.dovgaluk@gmail.com>
2018-12-26 11:28:53 +0300

    Color demo update

--- game/ColorDemo.cpp
+++ game/ColorDemo.cpp
@@ -17,9 +17,17 @@ static ColorDemoData* data;
 static void ColorDemo_prepare()
 {
     game_enable_frame_buffer();
-    for (int c = 0 ; c < WIDTH ; ++c)
-        for (int r = 0 ; r < HEIGHT ; ++r)
-            game_draw_pixel(c, r, (c / (WIDTH / 8)) * 16 + (r / (HEIGHT / 8)));
+    for (int c = 0 ; c < 8 ; ++c)
+        for (int r = 0 ; r < 8 ; ++r)
+        {
+            game_draw_rect(c * 8, r * 8, 8, 8, (r << 4) + c);
+            for (int i = 0 ; i < 4 ; ++i)
+                if (r & (1 << i))
+                    game_draw_pixel(c * 8 + i, r * 8, 0);
+            for (int i = 0 ; i < 4 ; ++i)
+                if (c & (1 << i))
+                    game_draw_pixel(c * 8 + i, r * 8 + 1, 0);
+        }
 }
 
 static void ColorDemo_render()
